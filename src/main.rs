29521d6eea55ea663b1d05dc7ea250d1716748b4
Tiago Krebs <krebstiago@gmail.com>
2022-10-16 15:44:01 -0300

    add Database Struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     // get args
     let mut arguments = std::env::args().skip(1);
@@ -7,7 +9,34 @@ fn main() {
     let value = arguments.next().unwrap();
     println!("The key is '{}' and the value is {}", key, value);
 
-    //file writing
     let contents = format!("{}\t{}\n", key, value);
-    std::fs::write("kv.db", contents);
+    // write file or panic!
+    std::fs::write("kv.db", contents).unwrap();
+
+    let database = Database::new().expect("Database::new() panic!");
+}
+
+struct Database {
+    map: HashMap<String, String>,
+}
+
+impl Database {
+    fn new() -> Result<Database, std::io::Error> {
+        // // read kv.db
+        // let contents = match std::fs::read_to_string("kv.db") {
+        //     Ok(c) => c,
+        //     Err(error) => {
+        //         return Err(error);
+        //     }
+        // };
+        // // same as above because ? operator
+        let contents = std::fs::read_to_string("kv.db")?;
+
+        // parse the string
+
+        // populate the map
+        Ok(Database {
+            map: HashMap::new(),
+        })
+    }
 }
